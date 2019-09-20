f383278afc6d91efe5ed4d2be424ff344472096d
nugattiStar <nugattistjerna@Protonmail.com>
2019-09-20 23:44:48 +0200

    Update settings file

--- settings_example.py
+++ settings_example.py
@@ -1,14 +1,9 @@
-##################Settings unt setup #################
-from deribit_api import RestClient
 
+# from deribit_api import RestClient
+# from rejson import Client, Path
+import json
+##################Settings##################
 
-login = dict(
-	Testnet = True,	##only change if you are feeling brave
-	Key = "",
-	Secret = "",
-        instrument = ["BTC-PERPETUAL", "BTC-28JUN19", "ETH-PERPETUAL"]
-
-)
 
 inputs = dict(
         buy = 'b',
@@ -20,16 +15,57 @@ inputs = dict(
         lessCorntracts = 'h',
         vimMode = 'o'
         # osv.....
+
 )
 
 
-if login['Testnet']:
-    httpcnnct = "https://test.deribit.com"
-    wscnt = 'wss://test.deribit.com/ws/api/v1/'
-else:
-    httpcnnct = "https://test.deribit.com"
-    wscnt = 'wss://test.deribit.com/ws/api/v1/'
+
+def msg(msg ,param, transid): 
+    output = {
+    "jsonrpc" : "2.0",
+    "id" : transid,
+    "method" : f"private/{msg}",
+    "params" : param
+    }
+    return output
 
 
-client = RestClient(login['Key'], login['Secret'], httpcnnct)
+def buySell(market, amount, price ,side): 
+    order = {
+      "jsonrpc" : "2.0",
+      "id" : 5275,
+      "method" : f"private/{side}",
+      "params" : {
+        "instrument_name" : "BTC-PERPETUAL",
+        "amount" : amount,
+        "type" : market,
+        "price": price,
+        "label" : "market0000234"
+      }
+    }
+    return json.dumps(order)
+
+
+
+
+auth = {
+  
+        "jsonrpc" : "2.0",
+        "id" : 9929,
+        "method" : "public/auth",
+        "params" : {
+            "grant_type" : "client_credentials",
+            "client_id" : "A7fcDnN2rqkb",
+            "client_secret" : "RVAIJ6Y3CQ6ZWONAB5X52OMXQWUALFTZ"}
+    }
+
+def sub(activeInst, *token):
+    output = {
+        "jsonrpc" : "2.0",
+        "id" : "sub949",
+        "method" : "private/subscribe",
+        "params" : { "channels" : [f"ticker.{activeInst}.100ms",f"trades.{activeInst}.raw"], 
+        "access_token": token}    
+    }
+    return output 
 
